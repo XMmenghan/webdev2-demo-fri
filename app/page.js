83fb7d151b9136f660f3b6570b635d015427b3b8
XMmenghan <96541054+XMmenghan@users.noreply.github.com>
2024-09-13 16:21:56 -0600

    Remove !

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function page(){
     return(
       <main>
         <h1>Web Dev 2 Demos</h1>
-        <p>These are the demos for web Dev 2!</p>
+        <p>These are the demos for web Dev 2?</p>
         <ul>
           <li><a href="/week-2">Week 2</a></li>
         </ul>
