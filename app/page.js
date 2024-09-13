fdd9bc8e5c71f293922b57a0c159d7e34639d442
XMmenghan <96541054+XMmenghan@users.noreply.github.com>
2024-09-13 16:00:11 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function page(){
     return(
       <main>
         <h1>Web Dev 2 Demos</h1>
-        <p>These are the demos for web Dev 2.</p>
+        <p>These are the demos for web Dev 2!</p>
       </main>
     )
 }
